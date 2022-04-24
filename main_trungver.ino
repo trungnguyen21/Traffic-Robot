18a124ec81e283565226fdeda9a02fee62ca6bb7
Trung Nguyen <atrung380@gmail.com>
2022-04-24 15:27:37 +0700

    24-4 commit

--- main_trungver.ino
+++ main_trungver.ino
@@ -1,23 +1,36 @@
+#include "UltraSonicDistanceSensor.h"
+
 int motor1Positive = 5;       //2 động cơ bên phải
 int motor1Negative = 4;
-
 int motor2Positive = 3;       //2 động cơ bên trái
 int motor2Negative = 2;
 
 int callValue;
-#define call 30
+#define call 30 // call_2 = 28
 
-int  giatri1 ; //Đọc giá trị cảm biến s1 và gán vào biến giatri1
-int  giatri2;
-int  giatri3;
-int  giatri4;
-int  giatri5;
 int signal;
+
+// sensor 2 ben
+int leftTrig = 49;
+int leftEcho = 48;
+int rightTrig = 51;
+int rightEcho = 50;
 
 int distanceLeft;
 int distanceRight;
 int distanceFront;
 int distanceBack;
+
+// sensor truoc sau
+const int trigBack = 53; //chan trig sau
+const int echoBack = 52; // chan echo sau
+const int trigFront = 47; // chan trig truoc
+const int echoFront = 46; // chan echo truoc
+
+UltraSonicDistanceSensor distanceBack(trigBack, echoBack);  // Initialize sensor that uses digital pins 43 and 42
+UltraSonicDistanceSensor distanceFront(trigFront, echoFront);  // Initialize sensor that uses digital pins 45 and 44
+double valuecm1 = 0;
+double valuecm2 = 0;
 
 // do line
 int s1 = 11; // Cảm biến trái ngoài cùng
@@ -46,20 +59,64 @@ void setup() {
   pinMode(s4, INPUT);
   pinMode(s5, INPUT);
 
-  // cam bien sieu am (trc sau)
+  // cam bien sieu am (trc sau) - ko can
 
   // cam bien sieu am (2 ben)
+  pinMode(leftTrig, OUTPUT);
+  pinMode(rightTrig, OUTPUT);
+  digitalWrite(leftTrig, HIGH);
+  digitalWrite(rightTrig, HIGH);
+  pinMode(leftEcho, INPUT);
+  pinMode(rightEcho, INPUT);
 
 }
 
 void sensorFrontBack()
 {
-  // TO-DO
+  if (distanceBack.available() == true)
+  {
+      distanceBack = distanceBack.getcm();  
+      Serial.println(distanceBack);
+  }
+
+  if (distanceFront.available() == true)
+  {
+    distanceFront = distanceFront.getcm();
+    Serial.println(distanceFront);
+  }
 }
 
 void sensorLeftRight()
 {
-  // TO-DO
+  // Serial.print("Distance Left = ");
+  digitalWrite(leftTrig, LOW);
+  digitalWrite(leftTrig, HIGH);
+
+  unsigned long LowLevelTimeLeft  = pulseIn(leftEcho, LOW);
+
+  // Serial.print("Distance Right = ");
+  digitalWrite(rightTrig, LOW);
+  digitalWrite(rightTrig, HIGH);
+
+  unsigned long LowLevelTimeRight  = pulseIn(rightEcho, LOW);
+
+  if (LowLevelTimeRight >= 50000) {
+    Serial.println("Invalid");
+  } else {
+    distanceRight = LowLevelTimeRight / 50;
+    Serial.print(distanceRight);
+    Serial.println(" cm");
+  }
+  
+  if (LowLevelTimeLeft >= 50000) {
+    Serial.println("Invalid");
+  }
+  else
+  {
+    distanceLeft = LowLevelTimeLeft / 50;
+    Serial.print(distanceLeft);
+    Serial.println(" cm");
+  }
 }
 
 void turnRight() {
@@ -101,11 +158,11 @@ void loop()
 {
   sensorFrontBack();
   sensorLeftRight();
-  int  giatri1 = digitalRead(s1); //Đọc giá trị cảm biến s1 và gán vào biến giatri1
-  int  giatri2 = digitalRead(s2);
-  int  giatri3 = digitalRead(s3);
-  int  giatri4 = digitalRead(s4);
-  int  giatri5 = digitalRead(s5);
+  int giatri1 = digitalRead(s1); //Đọc giá trị cảm biến s1 và gán vào biến giatri1
+  int giatri2 = digitalRead(s2);
+  int giatri3 = digitalRead(s3);
+  int giatri4 = digitalRead(s4);
+  int giatri5 = digitalRead(s5);
 
   
   if ((digitalRead(call) == 0) || (distanceBack > 50) || ((distanceLeft < 50) && (distanceRight < 50)))
@@ -137,16 +194,16 @@ void loop()
 //  Serial.print("S4: ");
 //  Serial.print(giatri4); Serial.print("   ");
 //  Serial.println(digitalRead(call));
-  if (digitalRead(call) == 1 || (distanceBack < 50)) {
-    if ((giatri2 == 0) && (giatri3 == 0) && (giatri4 == 0)) {
-      Serial.print("Di thang");
-      goForward();
-    } else if ((giatri2 == 1) && (giatri3 == 1) && (giatri4 == 1)) {
-      stop();
-      Serial.println("stop1");
-    }
-  } else if (digitalRead(call) == 0) {
-    stop();
-    Serial.println("stop2");
-  }
+  // if (digitalRead(call) == 1 || (distanceBack < 50)) {
+  //   if ((giatri2 == 0) && (giatri3 == 0) && (giatri4 == 0)) {
+  //     Serial.print("Di thang");
+  //     goForward();
+  //   } else if ((giatri2 == 1) && (giatri3 == 1) && (giatri4 == 1)) {
+  //     stop();
+  //     Serial.println("stop1");
+  //   }
+  // } else if (digitalRead(call) == 0) {
+  //   stop();
+  //   Serial.println("stop2");
+  // }
 }
