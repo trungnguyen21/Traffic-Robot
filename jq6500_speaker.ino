749d9050a41788edd96d1d599d37f1f8253f6f3f
Trung Nguyen <37546053+trungnguyen21@users.noreply.github.com>
2022-04-28 14:55:15 +0700

    Add speaker JQ6500 ino file

---
+++ jq6500_speaker.ino
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <JQ6500_Serial.h>
+
+int tx = 27;
+int rx = 28;
+
+SoftwareSerial mySerial(tx, rx);
+JQ6500_Serial mp3(mySerial);
+
+void setup() {
+  // put your setup code here, to run once:
+  mySerial.begin(9600);
+  mp3.reset();
+  mp3.setVolume(30);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //insert dieu kien
+  mp3.play();
+}
