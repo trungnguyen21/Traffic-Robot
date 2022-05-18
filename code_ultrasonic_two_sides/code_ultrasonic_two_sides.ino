9cfa79bc12e1b35e63a3f0355566636c0714e02a
Drake-The-Duck <starlordpnbach@gmail.com>
2022-05-18 11:30:16 +0700

    ver 1.8

---
+++ code_ultrasonic_two_sides/code_ultrasonic_two_sides.ino
@@ -0,0 +1,62 @@
+int leftTrig = 49;
+int leftEcho = 48;
+int rightTrig = 51;
+int rightEcho = 50;
+
+unsigned int leftDistance, rightDistance;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(leftTrig, OUTPUT);
+  pinMode(rightTrig, OUTPUT);
+  digitalWrite(leftTrig, HIGH);
+  digitalWrite(rightTrig, HIGH);
+  pinMode(leftEcho, INPUT);
+  pinMode(rightEcho, INPUT);
+  delay(500);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Distance Left = ");
+  digitalWrite(leftTrig, LOW);
+  digitalWrite(leftTrig, HIGH);
+
+  unsigned long LowLevelTimeLeft  = pulseIn(leftEcho, LOW);
+  if (LowLevelTimeLeft >= 50000) {
+    Serial.println("Invalid");
+  }
+  else
+  {
+    leftDistance = LowLevelTimeLeft / 50;
+    Serial.println(LowLevelTimeLeft);
+    Serial.print(leftDistance);
+    Serial.println(" cm");
+  }
+  
+  Serial.print("Distance Right = ");
+  digitalWrite(rightTrig, LOW);
+  digitalWrite(rightTrig, HIGH);
+
+  unsigned long LowLevelTimeRight  = pulseIn(rightEcho, LOW);
+
+  if (LowLevelTimeRight >= 50000) {
+    Serial.println("Invalid");
+  } else {
+    rightDistance = LowLevelTimeRight / 50;
+    Serial.println(LowLevelTimeRight);
+    Serial.print(rightDistance);
+    Serial.println(" cm");
+  }
+  
+
+
+//  if ((leftDistance <= 600) && (leftDistance >= 200) || (rightDistance <= 600) && (rightDistance >= 200)) {
+//    stop();
+//  } else {
+//    goForward();
+//  }
+  
+  delay(500);
+}
---
+++ sensors_truoc_sau/sensors_truoc_sau.ino
@@ -0,0 +1,31 @@
+#include "UltraSonicDistanceSensor.h"
+
+const int trigBack = 53; //chan trig sau
+const int echoBack = 52; // chan echo sau
+const int trigFront = 47; // chan trig truoc
+const int echoFront = 46; // chan echo truoc
+
+UltraSonicDistanceSensor distanceBack(trigBack, echoBack);  // Initialize sensor that uses digital pins 43 and 42
+UltraSonicDistanceSensor distanceFront(trigFront, echoFront);  // Initialize sensor that uses digital pins 45 and 44
+double valuecm1 = 0;
+double valuecm2 = 0;
+void setup () {
+  Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
+
+}
+
+void loop () {
+    // Every 100 miliseconds, do a measurement using the sensor and print the distance in centimeters.
+    if(distanceBack.available() == true){
+        valuecm1 = distanceBack.getcm();
+
+        Serial.println(valuecm1);
+    }
+    if(distanceFront.available() == true)
+    {
+      valuecm2 = distanceFront.getcm();
+      Serial.println(valuecm2);
+
+    }
+    delay(500);
+}
